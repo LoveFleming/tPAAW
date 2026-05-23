b0daafe097751b1a8d0d508005d39b3c15d235aa
LoveFleming <fleming7612@gmail.com>
2026-05-24 02:52:26 +0800

    fix: rename sidebar bottom button to 'Switch Working Base'

--- core/src/App.tsx
+++ core/src/App.tsx
@@ -454,7 +454,7 @@ function AppInner() {
             </SidebarSection>
           </div>
 
-          {/* Select Working Base */}
+          {/* Switch Working Base */}
           <div className="px-3 py-2 border-t shrink-0" style={{ borderColor: themeInfo.accentBorder + "60" }}>
             <button
               onClick={() => handlePickWorkingBase()}
@@ -466,7 +466,7 @@ function AppInner() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3.5 h-3.5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
               </svg>
-              Select Working Base
+              Switch Working Base
             </button>
           </div>
         </aside>
